ef71dbff658e5f75eb8d72383059efef7a27a197
Samanyu13 <samanyusaji18@gmail.com>
2026-06-30 00:53:23 +0530

    Fix desktop app dependency missing issue

--- HelloKMP/shared/src/jvmMain/kotlin/com/example/hellokmp/di/Modules.jvm.kt
+++ HelloKMP/shared/src/jvmMain/kotlin/com/example/hellokmp/di/Modules.jvm.kt
@@ -3,11 +3,14 @@ package com.example.hellokmp.di
 import com.example.hellokmp.BatteryManager
 import com.example.hellokmp.BatteryManagerImpl
 import com.example.hellokmp.dependencies.DbClient
+import com.example.hellokmp.dependencies.TestViewModel
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 actual val platformModule = module {
     singleOf(::DbClient)
+    viewModelOf(::TestViewModel)
     singleOf(::BatteryManagerImpl).bind<BatteryManager>()
 }
